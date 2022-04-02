618e54abee1346a082a114aa110a5b89771f580a
SennG <senn.geerts@otnsystems.com>
2022-04-02 19:39:44 +0200

    Example

---
+++ PubSub.Local.Test/Example.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PubSub.Api;
+
+namespace PubSub.Local.Test
+{
+    [TestFixture]
+    internal class Example
+    {
+        [Test]
+        public async Task Test()
+        {
+            IPubSub pubSub = new LocalPubSub();
+
+            await pubSub.Subscribe<string>(d => Console.WriteLine($"Received: {d}"));
+
+            await pubSub.Publish("Hello world");
+
+            using (var scope = pubSub.CreateScope())
+            {
+                await scope.Publish("Scoped 1");
+                await scope.Commit();
+
+                await scope.Publish("Scoped 2 -- will never be committed.");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # gRpc-PubSub
+
+.Net pub/sub library
+
+## Example
+```c#
+[Test]
+public async Task Test()
+{
+    IPubSub pubSub = new LocalPubSub();
+
+    await pubSub.Subscribe<string>(d => Console.WriteLine($"Received: {d}"));
+
+    await pubSub.Publish("Hello world");
+
+    using (var scope = pubSub.CreateScope())
+    {
+        await scope.Publish("Scoped 1");
+        await scope.Commit();
+
+        await scope.Publish("Scoped 2 -- will never be committed.");
+    }
+}
+```
